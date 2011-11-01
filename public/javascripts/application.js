3a4cbda1d59641ee8d3c7615c2bdab6b19d3d198
Ivan Rostovsky <ir@anahoret.com>
2011-11-01 11:52:42 +0200

    Minor convinient

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,16 @@
 $(function(){
   
-  $('a.swap').click(function(){
-    $('.first').swap('.third');
+  var changePlace = function(from, to){
+    $(from).swap(to);
     return false;
+  }
+  
+  $('a.swap_red_blue').click(function(){
+    return changePlace('.red', '.blue');
+  });
+  
+  $('a.swap_red_black').click(function(){
+    return changePlace('.red', '.black');
   });
   
 });
