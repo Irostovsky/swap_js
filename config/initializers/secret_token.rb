c7b1c25737a995014ea5cfc1e38b81c27c274ffc
Ivan Rostovsky <ir@anahoret.com>
2011-11-01 10:31:23 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwapJs::Application.config.secret_token = '1ee2e010e213bc04b1d37374ab9ff320f770d4c1bf99c628d412d2436fbb4115f3bc34183b501dd5716e74411bc0319471ef93b1a44813b5bc4f99d91f689265'
